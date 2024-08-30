13051f3d2c79b3fc540036d09cb5aae53c335074
Junhyeok2004Dec <junhyeok1209@outlook.kr>
2024-08-30 09:15:56 +0900

    20240830 - Point follower completed

--- src/NodeControl.cpp
+++ src/NodeControl.cpp
@@ -4,14 +4,14 @@
 #include <geometry_msgs/PointStamped.h>
 #include <nav_msgs/Odometry.h>
 #include <cmath>
-#include <map>
+#include <vector>
 
-std::map<uint32_t, geometry_msgs::PointStamped> waypoint_map;
-uint32_t last_seq = 0;
+std::vector<geometry_msgs::PointStamped> waypoints;
+int current_waypoint_index = 0;
 
 void pointCallback(const geometry_msgs::PointStamped::ConstPtr &msg) {
-    waypoint_map[msg->header.seq] = *msg;
-    ROS_INFO("Received waypoint %d: x = %f, y = %f", msg->header.seq, msg->point.x, msg->point.y);
+    waypoints.push_back(*msg);
+    ROS_INFO("Received waypoint %ld: x = %f, y = %f", waypoints.size(), msg->point.x, msg->point.y);
 }
 
 float distanceToGoal(const geometry_msgs::Point &goal, const geometry_msgs::Point &current_position) {
@@ -23,24 +23,26 @@ float computeSteeringAngle(const geometry_msgs::Point &goal, const geometry_msgs
 }
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr &msg, ros::Publisher &drive_pub) {
-    if (!waypoint_map.empty() && waypoint_map.count(last_seq + 1)) {
+    if (current_waypoint_index < waypoints.size()) {
         geometry_msgs::Point current_position = msg->pose.pose.position;
-        auto target = waypoint_map[last_seq + 1];
-        float dist = distanceToGoal(target.point, current_position);
+        auto target = waypoints[current_waypoint_index].point;
+        float dist = distanceToGoal(target, current_position);
 
         ROS_INFO("Current Position: x = %f, y = %f", current_position.x, current_position.y);
         ROS_INFO("Distance to target: %f", dist);
 
         if (dist < 0.1) {
-            last_seq++;
-            ROS_INFO("Reached waypoint %d, moving to next", last_seq);
+            current_waypoint_index++;
+            ROS_INFO("Reached waypoint %d, moving to next", current_waypoint_index);
         } else {
             ackermann_msgs::AckermannDriveStamped drive_msg;
             drive_msg.drive.speed = 1.0;
-            drive_msg.drive.steering_angle = computeSteeringAngle(target.point, current_position);
+            drive_msg.drive.steering_angle = computeSteeringAngle(target, current_position);
             ROS_INFO("Steering angle: %f", drive_msg.drive.steering_angle);
             drive_pub.publish(drive_msg);
         }
+    } else {
+        ROS_INFO("All waypoints reached.");
     }
 }
 
@@ -49,10 +51,8 @@ int main(int argc, char **argv) {
     ros::NodeHandle nh;
 
     ros::Publisher drive_pub = nh.advertise<ackermann_msgs::AckermannDriveStamped>("/drive", 100);
-    ros::Subscriber point_sub = nh.subscribe("/target_point", 100, pointCallback);
+    ros::Subscriber point_sub = nh.subscribe("/clicked_point", 100, pointCallback);
     ros::Subscriber odom_sub = nh.subscribe<nav_msgs::Odometry>("/odom", 100, boost::bind(odomCallback, _1, boost::ref(drive_pub)));
-  
-    ros::Rate loop_rate(100);  // 10 Hz loop rate
 
     ros::spin();
     return 0;
