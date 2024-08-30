85435f571b869f78bb960d80e6bb9a4bf6799d6f
Junhyeok2004Dec <junhyeok1209@outlook.kr>
2024-08-30 09:05:46 +0900

    20240830G

--- src/GoStraight5Sec.cpp
+++ src/GoStraight5Sec.cpp
@@ -6,12 +6,12 @@ int main(int argc, char **argv) {
     ros::NodeHandle nh;
 
     ros::Publisher drive_pub = nh.advertise<ackermann_msgs::AckermannDriveStamped>("/drive", 10);
-    
+    ros::Rate loop_rate(10);  // 10 Hz loop rate
+
     ackermann_msgs::AckermannDriveStamped drive_msg;
-    drive_msg.drive.speed = 1.0;  // Set a speed of 1.0 m/s
+    drive_msg.drive.speed = 1.4;  // Set a speed of 1.0 m/s
     drive_msg.drive.steering_angle = 0.0;  // No steering, straight
 
-    ros::Rate loop_rate(10);  // 10 Hz loop rate
     ros::Time start_time = ros::Time::now();
 
     while (ros::ok() && (ros::Time::now() - start_time).toSec() < 5.0) {
@@ -22,8 +22,17 @@ int main(int argc, char **argv) {
     }
 
     // Stop the car after 5 seconds
-    drive_msg.drive.speed = 0.0;
-    drive_pub.publish(drive_msg);
 
+    drive_msg.drive.speed = 0.0;
+    
+    start_time = ros::Time::now();
+
+    while (ros::ok() && (ros::Time::now() - start_time).toSec() < 5.0) {
+    
+
+    drive_pub.publish(drive_msg);
+    ros::spinOnce();
+    loop_rate.sleep();
+    }
     return 0;
 }
--- src/NodeControl.cpp
+++ src/NodeControl.cpp
@@ -1,62 +1,59 @@
 #include <ros/ros.h>
+#include <tf/transform_datatypes.h>
 #include <ackermann_msgs/AckermannDriveStamped.h>
+#include <geometry_msgs/PointStamped.h>
 #include <nav_msgs/Odometry.h>
-#include <geometry_msgs/Point.h>
-#include <vector>
-#include <functional>
+#include <cmath>
+#include <map>
 
-// Waypoints
-std::vector<geometry_msgs::Point> waypoints = [] {
-    std::vector<geometry_msgs::Point> wps;
-    for (int i = 0; i < 5; ++i) {
-        geometry_msgs::Point p;
-        p.x = i * 1.0;  // 0,1,2,3,4
-        p.y = i * 1.0;  
-        wps.push_back(p);
-    }
-    return wps;
-}();
+std::map<uint32_t, geometry_msgs::PointStamped> waypoint_map;
+uint32_t last_seq = 0;
 
-int current_waypoint = 0;
-geometry_msgs::Point current_position;
-
-void odomCallback(const nav_msgs::Odometry::ConstPtr &msg) {
-    current_position = msg->pose.pose.position;
+void pointCallback(const geometry_msgs::PointStamped::ConstPtr &msg) {
+    waypoint_map[msg->header.seq] = *msg;
+    ROS_INFO("Received waypoint %d: x = %f, y = %f", msg->header.seq, msg->point.x, msg->point.y);
 }
 
-float distanceToGoal(const geometry_msgs::Point &goal) {
+float distanceToGoal(const geometry_msgs::Point &goal, const geometry_msgs::Point &current_position) {
     return sqrt(pow(goal.x - current_position.x, 2) + pow(goal.y - current_position.y, 2));
 }
 
-float computeSteeringAngle(const geometry_msgs::Point &goal) {
+float computeSteeringAngle(const geometry_msgs::Point &goal, const geometry_msgs::Point &current_position) {
     return atan2(goal.y - current_position.y, goal.x - current_position.x);
 }
 
-int main(int argc, char **argv) {
-    ros::init(argc, argv, "waypoint_follower");
-    ros::NodeHandle nh;
+void odomCallback(const nav_msgs::Odometry::ConstPtr &msg, ros::Publisher &drive_pub) {
+    if (!waypoint_map.empty() && waypoint_map.count(last_seq + 1)) {
+        geometry_msgs::Point current_position = msg->pose.pose.position;
+        auto target = waypoint_map[last_seq + 1];
+        float dist = distanceToGoal(target.point, current_position);
 
-    ros::Publisher drive_pub = nh.advertise<ackermann_msgs::AckermannDriveStamped>("/drive", 10);
-    ros::Subscriber odom_sub = nh.subscribe("/odom", 10, odomCallback);
+        ROS_INFO("Current Position: x = %f, y = %f", current_position.x, current_position.y);
+        ROS_INFO("Distance to target: %f", dist);
 
-    ros::Rate loop_rate(10);
-
-    while (ros::ok() && current_waypoint < waypoints.size()) {
-        ackermann_msgs::AckermannDriveStamped drive_msg;
-
-        geometry_msgs::Point goal = waypoints[current_waypoint];
-        
-        if (distanceToGoal(goal) < 0.1) {  // Close to the waypoint
-            current_waypoint++;  // Move to the next waypoint
+        if (dist < 0.1) {
+            last_seq++;
+            ROS_INFO("Reached waypoint %d, moving to next", last_seq);
         } else {
-            drive_msg.drive.speed = 1.0;  // Set desired speed
-            drive_msg.drive.steering_angle = computeSteeringAngle(goal);  // Set steering angle
+            ackermann_msgs::AckermannDriveStamped drive_msg;
+            drive_msg.drive.speed = 1.0;
+            drive_msg.drive.steering_angle = computeSteeringAngle(target.point, current_position);
+            ROS_INFO("Steering angle: %f", drive_msg.drive.steering_angle);
             drive_pub.publish(drive_msg);
         }
-
-        ros::spinOnce();
-        loop_rate.sleep();
     }
+}
 
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "point_follower");
+    ros::NodeHandle nh;
+
+    ros::Publisher drive_pub = nh.advertise<ackermann_msgs::AckermannDriveStamped>("/drive", 100);
+    ros::Subscriber point_sub = nh.subscribe("/target_point", 100, pointCallback);
+    ros::Subscriber odom_sub = nh.subscribe<nav_msgs::Odometry>("/odom", 100, boost::bind(odomCallback, _1, boost::ref(drive_pub)));
+  
+    ros::Rate loop_rate(100);  // 10 Hz loop rate
+
+    ros::spin();
     return 0;
 }
