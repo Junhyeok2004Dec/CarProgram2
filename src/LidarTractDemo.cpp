b05bf33b8835146f042fc4a69ecdc18c3d64108f
Junhyeok2004Dec <junhyeok1209@outlook.kr>
2024-08-30 15:31:33 +0900

    240830 - backup -> making Car to tract lidar data

--- src/LidarTractDemo.cpp
+++ src/LidarTractDemo.cpp
@@ -1,4 +1,6 @@
 #include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <std_msgs/Float32.h>
 #include <tf/transform_datatypes.h>
 #include <ackermann_msgs/AckermannDriveStamped.h>
 #include <geometry_msgs/PointStamped.h>
@@ -6,14 +8,32 @@
 #include <cmath>
 #include <vector>
 
-static float COLLISION_RANGE = 1.4f; 
 
-std::vector<geometry_msgs::PointStamped> waypoints;
+struct Point 
+{
+    std_msgs::Float32 x;
+    std_msgs::Float32 y;
+};
+
+Point destination; // 도착지점 -> scan_angle 방위의 scan_distance 거리 
+Point currentPOS; // 현재지점 -> 제곧내
+
+static float COLLISION_RANGE = 1.0f; 
+
 ackermann_msgs::AckermannDriveStamped drive_data; //현재 주행 상태 get
 int current_waypoint_index = 0;
 
+std_msgs::Float32 scan_length_msg, scan_angle_msg;
 
 
+void distanceCallback(const std_msgs::Float32 &msg) {
+    destination.x = &msg;
+
+}
+
+void angleCallback(const std_msgs::Float32 &msg) {
+    destination.y = &msg;
+}
 
 void pointCallback(const geometry_msgs::PointStamped::ConstPtr &msg) {
     waypoints.push_back(*msg);
@@ -39,31 +59,34 @@ float computeSteeringAngle(const geometry_msgs::Point &goal, const geometry_msgs
 
 //::TODO :: deadman-switch 생성 -> 일정 시간 이상동안 도달하지 않으면 다음 waypoint로 파견.
 void odomCallback(const nav_msgs::Odometry::ConstPtr &msg, ros::Publisher &drive_pub) {
-    if (current_waypoint_index < waypoints.size()) {
-        geometry_msgs::Point current_position = msg->pose.pose.position;
-        auto target = waypoints[current_waypoint_index].point;
-        float dist = distanceToGoal(target, current_position);
 
-        ROS_INFO("Current Position: x = %f, y = %f", current_position.x, current_position.y);
-        ROS_INFO("Distance to target: %f", dist);
 
-        if (dist < COLLISION_RANGE) {
-            current_waypoint_index++; // 도착했습니다!
-            ROS_INFO("Reached waypoint %d, moving to next", current_waypoint_index);
+    geometry_msgs::Point current_position = msg->pose.pose.position;
+    //auto target = waypoints[current_waypoint_index].point;
+
+    auto targer = 
+    float dist = distanceToGoal(target, current_position);
+
+    ROS_INFO("Current Position: x = %f, y = %f", current_position.x, current_position.y);
+    ROS_INFO("Distance to target: %f", dist);
+
+    if (dist < COLLISION_RANGE) {
+        current_waypoint_index++; // 도착했습니다!
+        ROS_INFO("Reached waypoint %d, moving to next", current_waypoint_index);
+    }
+    
+        else {
+        ackermann_msgs::AckermannDriveStamped drive_msg;
+
+        drive_msg.drive.speed = 1.0;
+        drive_msg.drive.steering_angle = computeSteeringAngle(target, current_position);           
+        ROS_INFO("Steering angle: %f", drive_msg.drive.steering_angle);
+        drive_pub.publish(drive_msg);
+
+        if(drive_msg.drive.steering_angle == drive_data.drive.steering_angle) {
+            ROS_INFO("yeeh!");
         }
-        
-         else {
-            ackermann_msgs::AckermannDriveStamped drive_msg;
 
-            drive_msg.drive.speed = 1.0;
-            drive_msg.drive.steering_angle = computeSteeringAngle(target, current_position);           
-            ROS_INFO("Steering angle: %f", drive_msg.drive.steering_angle);
-            drive_pub.publish(drive_msg);
-
-            if(drive_msg.drive.steering_angle == drive_data.drive.steering_angle) {
-                ROS_INFO("yeeh!");
-            }
-        }
     } else {
 
 
@@ -81,13 +104,17 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "point_follower");
     ros::NodeHandle nh;
 
+    destination = {x=0.0, y=0.0};
+    currentPOS = {x=-1.0, y=-1.0};
+
     ros::Publisher drive_pub = nh.advertise<ackermann_msgs::AckermannDriveStamped>("/drive", 100);
     
     ros::Subscriber drive_sub = nh.subscribe("/drive", 100, driveCallback);
-    ros::Subscriber point_sub = nh.subscribe("/clicked_point", 100, pointCallback);
+    //ros::Subscriber point_sub = nh.subscribe("/clicked_point", 100, pointCallback);
+    ros::Subscriber scan_sub_length = nh.subscribe("/scan_demo_distance", 10, distanceCallback);
+    ros::Subscriber scan_sub_angle = nh.subscribe("/scan_demo_angle", 10, angleCallback);
+    
     ros::Subscriber odom_sub = nh.subscribe<nav_msgs::Odometry>("/odom", 100, boost::bind(odomCallback, _1, boost::ref(drive_pub)));
-
-
 
     ros::spin();
     return 0;
